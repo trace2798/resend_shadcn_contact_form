d9fa19cfc8eb1bd16b7736df58d86e3a71ad73b1
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-08-23 17:20:03 +0530

    added the toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { FeedbackForm } from "@/components/feedback-form";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-5">
+      <ModeToggle/>
       <h1 className="text-xl font-semibold">Feedback Form</h1>
       <FeedbackForm />
     </main>
